c197b862ef0ec6839967f82257cb478b7094fc3d
Arrogz <108223317+Arrogz@users.noreply.github.com>
2026-07-08 15:11:21 +1000

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <h1>THIS IS A TEST</h1>
+      <h2>CAN I?</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
